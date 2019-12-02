f7bef5d3f278aa6a72aa7618e8257477cd6065cb
Slawomir Golebiewski <slawomir.golebiewski@inspeerity.com>
2019-12-02 14:21:41 +0100

    User Model Class

---
+++ src/main/java/com/staxter/springrest/user/impl/User.java
@@ -0,0 +1,86 @@
+package com.staxter.springrest.user.impl;
+
+class User {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String plainTextPassword;
+    private String hashedPassword;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public String getPlainTextPassword() {
+        return plainTextPassword;
+    }
+
+    public void setPlainTextPassword(String plainTextPassword) {
+        this.plainTextPassword = plainTextPassword;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(user.lastName) : user.lastName != null) return false;
+        if (userName != null ? !userName.equals(user.userName) : user.userName != null) return false;
+        if (plainTextPassword != null ? !plainTextPassword.equals(user.plainTextPassword) : user.plainTextPassword != null)
+            return false;
+        return hashedPassword != null ? hashedPassword.equals(user.hashedPassword) : user.hashedPassword == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (userName != null ? userName.hashCode() : 0);
+        result = 31 * result + (plainTextPassword != null ? plainTextPassword.hashCode() : 0);
+        result = 31 * result + (hashedPassword != null ? hashedPassword.hashCode() : 0);
+        return result;
+    }
+}
