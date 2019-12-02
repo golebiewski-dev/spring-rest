dc1b5340daa470363784c6160d15f169bf93c3b2
Slawomir Golebiewski <slawomir.golebiewski@inspeerity.com>
2019-12-02 14:22:40 +0100

    User Dto Class is responsible for the communication between the service layer and the model

---
+++ src/main/java/com/staxter/springrest/user/UserDto.java
@@ -0,0 +1,86 @@
+package com.staxter.springrest.user;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotNull;
+
+public class UserDto {
+
+    private String id;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    private String userName;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserDto userDto = (UserDto) o;
+
+        if (id != null ? !id.equals(userDto.id) : userDto.id != null) return false;
+        if (firstName != null ? !firstName.equals(userDto.firstName) : userDto.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(userDto.lastName) : userDto.lastName != null) return false;
+        if (userName != null ? !userName.equals(userDto.userName) : userDto.userName != null) return false;
+        return password != null ? password.equals(userDto.password) : userDto.password == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (userName != null ? userName.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        return result;
+    }
+}
